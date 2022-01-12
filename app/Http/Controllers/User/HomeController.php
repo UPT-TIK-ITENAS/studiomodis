39f11cdf23facbaa8af0f1f4ed9655e623c75fac
Muqiit <mmuqiit.f14@gmail.com>
2022-01-12 12:11:08 +0700

    user-home

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -10,14 +10,12 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $peminjaman_ruangan = Borrow::with(['ruangan', 'alat'])->whereHas('ruangan')->where('user_id', auth()->user()->id);
-        $peminjaman_alat = Borrow::with(['ruangan', 'alat'])->whereDoesntHave('ruangan')->whereHas('alat')->where('user_id', auth()->user()->id);
-        $peminjaman_ruangan_m = $peminjaman_ruangan->where('status', 0)->count();
-        $peminjaman_ruangan_s = $peminjaman_ruangan->where('status', 1)->count();
-        $peminjaman_ruangan_t = $peminjaman_ruangan->where('status', 2)->count();
-        $peminjaman_alat_m = $peminjaman_alat->where('status', 0)->count();
-        $peminjaman_alat_s = $peminjaman_alat->where('status', 1)->count();
-        $peminjaman_alat_t = $peminjaman_alat->where('status', 2)->count();
+        $peminjaman_ruangan_m = Borrow::with(['ruangan', 'alat'])->whereHas('ruangan')->where('user_id', auth()->user()->id)->where('status', 0)->count();
+        $peminjaman_ruangan_s = Borrow::with(['ruangan', 'alat'])->whereHas('ruangan')->where('user_id', auth()->user()->id)->where('status', 1)->count();
+        $peminjaman_ruangan_t = Borrow::with(['ruangan', 'alat'])->whereHas('ruangan')->where('user_id', auth()->user()->id)->where('status', 2)->count();
+        $peminjaman_alat_m = Borrow::with(['alat'])->whereDoesntHave('ruangan')->whereHas('alat')->where('user_id', auth()->user()->id)->where('status', 0)->count();
+        $peminjaman_alat_s = Borrow::with(['alat'])->whereDoesntHave('ruangan')->whereHas('alat')->where('user_id', auth()->user()->id)->where('status', 1)->count();
+        $peminjaman_alat_t = Borrow::with(['alat'])->whereDoesntHave('ruangan')->whereHas('alat')->where('user_id', auth()->user()->id)->where('status', 2)->count();
         return view('user.home', compact('peminjaman_ruangan_m', 'peminjaman_ruangan_s', 'peminjaman_ruangan_t', 'peminjaman_alat_m', 'peminjaman_alat_s', 'peminjaman_alat_t'));
     }
 }
